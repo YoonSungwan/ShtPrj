558cc67acf95407528eb940cea6c2869c2bf13c9
swy <sungwanyoon@gmail.com>
2024-08-08 18:25:30 +0900

    Initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+# ShtPrj
+
+Developed with Unreal Engine 5
---
+++ Source/ShtPrj/ShtPrj.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShtPrj.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShtPrj, "ShtPrj" );
